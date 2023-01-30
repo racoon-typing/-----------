88605b09b0de3b365b5c7a4d28c3077b35e7f153
Daniil <eo18622@yandex.ru>
2023-01-30 14:22:28 +0300

    логика

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-const calculatorList = document.
+// Ноды
+const calculatorList = document.querySelector('.calculator__list');
+const outputContentNode = document.querySelector('.calculator__output-content');
+
+let firstString = '';
+let secondString = '';
+let thirddString = '';
+
+// Слушатель на нажатие
+calculatorList.addEventListener('click', (e) => {
+
+    // Проверка на пустую 1 строку
+    if (e.target.id === 'divide' || e.target.id === 'multiply' || e.target.id === 'minus' || e.target.id === 'plus' || e.target.id === 'equals' || e.target.id === 'del') {
+        if (firstString === '') {
+            console.log('Введите какой-нибудь символ');
+            return;
+        } else {
+            if (e.target.id === 'divide') {
+                secondString = '/';
+            } else if (e.target.id === 'multiply') {
+                secondString = '*';
+            } else if (e.target.id === 'minus') {
+                secondString = '-';
+            } else if (e.target.id === 'plus') {
+                secondString = '+';
+            } else {
+                firstString = '';
+                secondString = '';
+            }
+            console.log(secondString);
+
+        }
+    } else {
+        console.log(e.target.id);
+        firstString += e.target.id;
+
+    }
+    outputContentNode.textContent = firstString;
+
+
+
+    // else if (e.target.id === 'divide' || e.target.id === 'multiply' || e.target.id === 'minus' || e.target.id === 'plus') {
+    //     if (e.target.id === 'divide') {
+    //         secondString += '/';
+    //     } else if (e.target.id === 'multiply') {
+    //         secondString += '*';
+    //     } else if (e.target.id === 'minus') {
+    //         secondString += '-';
+    //     } else {
+    //         secondString += '+';
+    //     }
+    // } 
+
+    // if (secondString !== '') {
+
+    // }
+
+
+    // firstString += e.target.id;
+
+    // console.log(firstString);
+    // console.log(secondString);
+
+
+
+});
